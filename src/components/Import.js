65c7e51c8f9805c9789bc2658b2bff9dd0b2bbf1
David Bille <d_bille@yahoo.com>
2020-02-13 19:51:54 -0600

    working on table

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell
+} from "@material-ui/core";
 
-const Import = (props) => {
-    // fill out this component
-
-    return (
-        <p>Import Component</p>
-    )
+function createData(id, make, actions) {
+  return { id, make, actions };
 }
 
-export default Import
+const rows = [createData("1", "honda", "add")];
+
+const Import = props => {
+  // fill out this component
+
+  return (
+    <div className="table-container">
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Make</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.id}>
+              <TableCell>{row.id}</TableCell>
+              <TableCell>{row.make}</TableCell>
+              <TableCell>{row.actions}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Button color="primary" variant="contained">
+        Import
+      </Button>
+    </div>
+  );
+};
+
+export default Import;
